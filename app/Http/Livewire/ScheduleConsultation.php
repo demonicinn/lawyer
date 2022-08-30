34b0a6a634e81f8803797dfbd7970e652d97554f
“dibyanshu11” <dibyanshu.ranjans@gmail.com>
2022-08-30 16:12:14 +0530

    working on time slot

--- app/Http/Livewire/ScheduleConsultation.php
+++ app/Http/Livewire/ScheduleConsultation.php
@@ -9,11 +9,17 @@ use App\Models\LawyerHours;
 
 class ScheduleConsultation extends Component
 {
-    public $lawyerID, $lawyer, $day, $avaibleTime, $selDate;
+    public $lawyerID, $lawyer, $day, $avaibleTime, $selDate, $lawyerDetails, $scheduletime;
+    public $timeSlots = [];
+    public $shedulePage = true;
+    public $clickConfirm = false;
 
     public function availability()
     {
+
+        // $this->day = 'monday';
         $this->day = Carbon::now()->format("l");
+
         $this->avaibleTime = Carbon::now()->format("l, F j ");
 
 
@@ -24,9 +30,51 @@ class ScheduleConsultation extends Component
             $this->day  = date('l', strtotime($this->selDate . ' +1 day'));
         }
         $user = LawyerHours::where('users_id', $this->lawyerID)->where('day', $this->day)->first();
-
         $this->lawyer = $user;
+
+        if ($this->lawyer != null) {
+            $starttime = $user->from_time; // your start time
+            $endtime = $user->to_time;  // End time
+            $duration = '30';  // split by 30 mins
+            $time_slots = array();
+            $start_time    = strtotime($starttime); //change to strtotime
+            $end_time      = strtotime($endtime); //change to strtotime
+
+            $add_mins  = $duration * 60;
+
+            while ($start_time <= $end_time) // loop between time
+            {
+                $time_slots[] = date("H:i", $start_time);
+                $start_time += $add_mins; // to check endtime
+            }
+
+            $this->timeSlots = $time_slots;
+            // dd($this->timeSlots);
+        }
     }
+
+    public function confirm()
+    {
+        $this->validate([
+            'scheduletime' => 'required|max:255',
+
+        ], [
+            'scheduletime.required' => 'please select time.'
+        ]);
+
+       
+
+        $this->clickConfirm = true;
+        $this->shedulePage = false;
+        $this->lawyerDetails = User::where('id', $this->lawyerID)->first();
+    }
+
+    public function backbtn()
+    {
+        $this->clickConfirm = false;
+        $this->shedulePage = true;
+    }
+
 
     public function render()
     {
