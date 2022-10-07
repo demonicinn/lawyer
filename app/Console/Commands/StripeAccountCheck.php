f3dfe3b99b0af2fb8e26779cf0918986eef724c2
Guri <info.gurpreetsaini@gmail.com>
2022-10-07 23:08:31 +0530

    laywer account

--- app/Console/Commands/StripeAccountCheck.php
+++ app/Console/Commands/StripeAccountCheck.php
@@ -3,22 +3,26 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Models\BankInfo;
 
 class StripeAccountCheck extends Command
 {
+
+    
+
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'lawyers:account';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Check Lawyers account activated or not';
 
     /**
      * Execute the console command.
@@ -27,6 +31,34 @@ class StripeAccountCheck extends Command
      */
     public function handle()
     {
-        return Command::SUCCESS;
+        $records = BankInfo::where(['payouts_enabled' => 'inactive'])->get();
+
+        try {
+            if($records) {
+                $stripe = new \Stripe\StripeClient(
+                    config('services.stripe.secret')
+                );
+
+                foreach($records as $record){
+                    $account = $stripe->accounts->retrieve($record->account_id);
+
+                    if(@$account && $account->payouts_enabled) {
+                        $record->payouts_enabled = 'active';              
+                    } else {
+                        $record->payouts_enabled = 'inactive'; 
+                    }
+                    
+                    $record->save();
+
+                    //$this->flash('success', 'Account added successfully');
+                }
+            }
+
+        } catch (\Exception $e) {
+            //$this->flash('error', $e->getMessage());
+        }
+
+        return 1;
+
     }
 }
