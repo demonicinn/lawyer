5a05e7b641b8be0d2df3d91d0f48fe55c04929d3
unknown <rohit@yopmail.com>
2022-11-08 13:41:52 +0530

    css333

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -38,6 +38,8 @@
           <p>Unlike other legal directories, we provide advanced filters that help narrow down which lawyer is right for you. We also provide the opportunity to easily schedule a video consultation with the lawyer to determine if they are the right fit. Additionally, we won’t ask for your personal information until you schedule a consultation. </p>
           <a href="#" class="btn-design-four">See The Difference</a>
         </div>
+
+        
         </div>
     <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12">
        <div class="why-choose-img text-center half-mobile-img">
